82a53a113b367568c37e956de2a9b1f34db3cea7
jdjebi <jeanmarcdjebi@gmail.com>
2024-02-09 16:14:58 +0100

    Corrections et ajouts de la récupération d'un extracteur en fonction de son nom

--- database/crud.py
+++ database/crud.py
@@ -5,11 +5,14 @@ from . import models, schemas
 def get_extractor(db: Session, extractor_id: int):
     return db.query(models.Extractor).filter(models.Extractor.id == extractor_id).first()
 
+def get_extractor_by_name(db: Session, extractor_name: str):
+    return db.query(models.Extractor).filter(models.Extractor.name == extractor_name).first()
+
 def get_all_extractors(db: Session):
     return db.query(models.Extractor).all()
 
 def create_extractor(db: Session, extractor: schemas.Extractor):
-    db_extractor = models.Extractor(**extractor.model_dump())
+    db_extractor = models.Extractor(**extractor.dict())
     db.add(db_extractor)
     db.commit()
     db.refresh(db_extractor)
