29ee8131c73f276d7ca026667308b0613eee9217
jdjebi <jeanmarcdjebi@gmail.com>
2024-02-09 16:16:04 +0100

    Mise à jour pour l'insertion d'un extracteur en base de données

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Body, Depends, FastAPI, HTTPException, Query
+from sqlalchemy.orm import Session
 
-from api.models.extractor import Extractor
-from database.database import SessionLocal, engine
+from database.database import engine
 from database import crud, models, schemas
+from database.session import get_db
 
+# TO DO : Utilise Alembic intégrer un système de migration
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -19,7 +22,15 @@ def get_extractor(id: int):
         "id": id
     }
 
-@app.post("/api/extractors")
-def create_extractor(extractor: schemas.Extractor):
-    extractor.id = 1
-    return extractor
+@app.post("/api/extractors", response_model=schemas.Extractor)
+def create_extractor(new_extractor: schemas.Extractor = Body(), 
+                     force_create_if_exist : bool = Query(default=False), 
+                     db: Session = Depends(get_db)
+                    ):
+
+    if force_create_if_exist:
+        extractor = crud.get_extractor_by_name(db, new_extractor.name)
+        if extractor:
+            raise HTTPException(status_code=400, detail="Nom de l'extracteur déjà utilisé.")
+
+    return crud.create_extractor(db, new_extractor)
